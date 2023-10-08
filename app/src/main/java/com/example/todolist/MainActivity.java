722438f564f125c6380e1aa06e9ff571cbb211b4
hawariMuflihMunte <hawari.muflih.munte@gmail.com>
2023-10-09 02:31:00 +0700

    Feat: set padding values for home_page_wrapper <LinearLayout>

--- app/src/main/java/com/example/todolist/MainActivity.java
+++ app/src/main/java/com/example/todolist/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.todolist;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Set padding for home_page_wrapper
+        View homePageWrapper = findViewById(R.id.home_page_wrapper);
+        homePageWrapper.setPadding(26, 26, 26, 26);
     }
 }
